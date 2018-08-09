a68a3e2936d00bdda411cb6f73c37f8bd0d70f71
jbcruz <35581373+juleskoderepo@users.noreply.github.com>
2018-08-09 13:04:58 -0500

    Add logic to insert dummy data to db

--- app/src/main/java/com/example/android/abndbookstoreinventory/InventoryActivity.java
+++ app/src/main/java/com/example/android/abndbookstoreinventory/InventoryActivity.java
@@ -1,15 +1,25 @@
 package com.example.android.abndbookstoreinventory;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.math.BigDecimal;
+
+import com.example.android.abndbookstoreinventory.data.ProductDbHelper;
+import com.example.android.abndbookstoreinventory.data.ProductContract.ProductEntry;
+
 public class InventoryActivity extends AppCompatActivity {
+
+    ProductDbHelper prodDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +36,13 @@ public class InventoryActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // Database helper to get access to the database
+        prodDbHelper = new ProductDbHelper(this);
+
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -42,11 +58,43 @@ public class InventoryActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(id){
+            case R.id.action_insert_dummy_data:
+                insertProduct();
+
+                return true;
+            case R.id.action_settings:
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Helper method to insert hardcoded values into db
+     */
+    private void insertProduct(){
+        // Get database in write mode
+        SQLiteDatabase db = prodDbHelper.getWritableDatabase();
+
+        // Create a map of values with column names as keys
+        ContentValues values = new ContentValues();
+        values.put(ProductEntry.COLUMN_PRODUCT_NAME, getString(R.string.dummy_name));
+
+        int price = Integer.parseInt(getString(R.string.dummy_price));
+        values.put(ProductEntry.COLUMN_PRICE, price);
+
+        int quantity = Integer.parseInt(getString(R.string.dummy_quantity));
+        values.put(ProductEntry.COLUMN_QUANTITY, quantity);
+
+        values.put(ProductEntry.COLUMN_SUPPLIER_NAME, getString(R.string.dummy_supplier_name));
+        values.put(ProductEntry.COLUMN_SUPPLIER_PHONE, getString(R.string.dummy_supplier_phone));
+
+        Log.i("InventoryActivity","Content values: " + values);
+        // Insert the new row, returning the primary key value of the new row i.e. _id
+        // Returns -1 if error occurs inserting data
+        long newRowId = db.insert(ProductEntry.TABLE_NAME, null, values);
+
+
     }
 }
